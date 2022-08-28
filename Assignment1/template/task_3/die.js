07beaf2bf820dadbb832d440b1167d66446a96e6
RedHawk <tadshig@gmail.com>
2022-08-29 00:15:25 +0300

    Assignment1. Failed attempt.

---
+++ Assignment1/template/task_3/die.js
@@ -0,0 +1,26 @@
+/**********************/
+/* Main Functionality */
+/**********************/
+
+function RollDie() {
+    let result = Math.floor(Math.random() * 6 + 1);
+	console.log('IT WORKS! ' + result);
+	//document.querySelector('#die_button').textContent = 'Hey' + result;
+    if ('URLSearchParams' in window) {
+        var searchParams = new URLSearchParams(window.location.search)
+        searchParams.set("roll", result);
+        var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+        history.pushState(null, '', newRelativePathQuery);
+    }
+}
+/*
+function UpdatePicture(result) {
+    document.querySelector('.die').src = '/image/' + result + '.png';
+}
+*/
+/********************/
+/* Button Listeners */
+/********************/
+
+const RollDieButton = document.querySelector('#die_button');
+RollDieButton.addEventListener('click', RollDie);
--- Assignment1/template/task_3/index.js
+++ Assignment1/template/task_3/index.js
@@ -2,21 +2,54 @@
 
 const path = require('path');
 const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+const { query } = require('express');
 
 const app = express();
 
+app.set('view engine','ejs');
+app.use(express.static('views'));
 
-// Express setup
+// Listen on port 3000. http://localhost:3000
+app.listen(3000, function () {
+    console.log('Listening port 3000');
+});
 
 
 app.get('/', (req, res) => {
-	// Send a landing page
+	res.sendFile(path.join(__dirname + '/views' + '/home.html'));
+	if(req.query.roll) {
+        res.render('die', {roll:req.query.roll});
+		//UpdatePicture(req.query.roll);
+    }
 });
 
-
-app.get('/image', (req, res) => {
-	// Return a die template
+app.get('/die.js', (req, res) => {
+	res.sendFile(path.join(__dirname + '/die.js'));
 });
 
+app.get('/images/1.png', (req, res) => {
+	res.sendFile(path.join(__dirname + '/images/1.png'));
+});
 
-app.listen(3000);
+app.get('/images/2.png', (req, res) => {
+	res.sendFile(path.join(__dirname + '/images/2.png'));
+});
+
+app.get('/images/3.png', (req, res) => {
+	res.sendFile(path.join(__dirname + '/images/3.png'));
+});
+
+app.get('/images/4.png', (req, res) => {
+	res.sendFile(path.join(__dirname + '/images/4.png'));
+});
+
+app.get('/images/5.png', (req, res) => {
+	res.sendFile(path.join(__dirname + '/images/5.png'));
+});
+
+app.get('/images/6.png', (req, res) => {
+	res.sendFile(path.join(__dirname + '/images/6.png'));
+});
