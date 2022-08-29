989a7d3d58fbe3ed96fcb1d99a49fd29f627e676
ReddishHawk <tadshig@gmail.com>
2022-08-29 09:40:00 +0300

    Assignment1. Done?

--- Assignment1/template/task_3/index.js
+++ Assignment1/template/task_3/index.js
@@ -3,64 +3,32 @@
 const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
-const bodyParser = require('body-parser');
-const res = require('express/lib/response');
-const { query } = require('express');
 
 const app = express();
 
 app.set('view engine','ejs');
 app.use(express.static('views'));
+app.use('/images', express.static('./images'));
 
 // Listen on port 3000. http://localhost:3000
 app.listen(3000, function () {
     console.log('Listening port 3000');
 });
 
-
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/views' + '/home.html'));
-	/*if(req.query.roll) {
-        //res.render('die', {roll:req.query.roll});
-		roll = req.query.roll;
-		res.redirect('/image');
-		//UpdatePicture(req.query.roll);
-    }*/
+	res.render('home');
 });
 
-app.get('/image', (req, res) => {
-	let result = Math.floor(Math.random() * 6 + 1);
-	res.render('die', {roll:result});
-});
-
-app.get('/die.js', (req, res) => {
-	res.sendFile(path.join(__dirname + '/die.js'));
-});
-
-app.get('/images/1.png', (req, res) => {
-	res.sendFile(path.join(__dirname + '/images/1.png'));
-});
-
-app.get('/images/2.png', (req, res) => {
-	res.sendFile(path.join(__dirname + '/images/2.png'));
-});
-
-app.get('/images/3.png', (req, res) => {
-	res.sendFile(path.join(__dirname + '/images/3.png'));
-});
-
-app.get('/images/4.png', (req, res) => {
-	res.sendFile(path.join(__dirname + '/images/4.png'));
-});
-
-app.get('/images/5.png', (req, res) => {
-	res.sendFile(path.join(__dirname + '/images/5.png'));
-});
-
-app.get('/images/6.png', (req, res) => {
-	res.sendFile(path.join(__dirname + '/images/6.png'));
+app.get('/:image', (req, res) => {
+	let result = req.params.image;
+	if (!isNaN(result) && result > 0 && result < 7) {
+		res.render('die', {error:0, roll:result});
+	} else {
+		res.render('die', {error:1, roll:0});
+	}
+	
 });
 
 app.get('/static/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views' + '/style.css'));
+    res.sendFile(path.join(__dirname + '/views/style.css'));
 });
