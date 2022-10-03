e2ecfbba277f187a1e923ef326fba0a9d306cb11
ReddishHawk <tadshig@gmail.com>
2022-10-03 14:01:20 +0300

    Assignment4. Task3. Done.

--- Assignment4/task_3/index.js
+++ Assignment4/task_3/index.js
@@ -20,8 +20,7 @@ const com = new SerialPort({path: 'COM5', baudRate: 115200});
 
 const parser = com.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-let HELP_ME = 0;
-
+let temp_late = 0;
 let client;
 mongo.connect('mongodb://localhost:27017/', function (error,db) {
 	if(error) throw error;
@@ -38,7 +37,7 @@ app.listen(3002, function () {
 });
 
 parser.on('data', data => {
-    HELP_ME = JSON.parse(data);
+    temp_late = JSON.parse(data);
     let msr = JSON.parse(data);
     client.collection('temp_mes').insertOne(msr, function (error, res) {
         if (error) throw error;
@@ -48,7 +47,24 @@ parser.on('data', data => {
 
 
 app.get('/', (req,res) => {
-    if (HELP_ME) res.render('home', {temp: HELP_ME.temperature});
-    else res.render('home', {temp: 0});
-    console.log(HELP_ME);
+    try {
+		client.collection('temp_mes').find({}).toArray(function (error,result) {
+			if(error) throw error;
+            let measurements = JSON.parse(JSON.stringify(result));
+			let ave_temp = 0;
+            let i = 0;
+            let max = -500;
+            let min = 500;
+            for (let msr of measurements) {
+                ave_temp += msr.temperature;
+                i++;
+                if (min > msr.temperature) min = msr.temperature;
+                if (max < msr.temperature) max = msr.temperature;
+            }
+            ave_temp /= i;
+            res.render('home', {temp: temp_late.temperature, max_temp: max, min_temp: min, ave_temp: ave_temp});
+		});
+	} catch (error) {
+		res.status(500).send('Unable to get measurements.');
+	}
 });
