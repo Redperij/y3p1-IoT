b8b0c3966427b29fad9ab023cc34cc3ab3954ec1
ReddishHawk <tadshig@gmail.com>
2022-10-10 11:40:09 +0300

    Assignment5. Task1. WORKS!

--- Assignment5/task1/script.js
+++ Assignment5/task1/script.js
@@ -14,33 +14,41 @@ const colorLerp = (color1, color2, percentage) => {
 		(color2 & 0xff00) - gStart,
 		(color2 & 0xff) - bStart];
 
-	return '#' + (Math.floor(rStart + rDiff * percentage) & 0xff0000
+    return '#' + (Math.floor(rStart + rDiff * percentage) & 0xff0000
 			| Math.floor(gStart + gDiff * percentage) & 0xff00
 			| Math.floor(bStart + bDiff * percentage) & 0xff).toString(16).padStart(6, '0');
 }
 
 function applySVG(temp) {
     const svgDoc = svgThermometer.contentDocument;
-    if (temp === 0) {
-        //svgThermometer.getElementById('bulb').setAttribute('fill', 'rgb(0,0,0)');
-        //svgThermometer.setAttributeNS(null, bulb, rgb(0,0,0));
-        //svgThermometer.style.fill = '#000';
-        //document.getElementById('bulb').setAttributeNS(null, fill, rgb(0,0,0));
-        svgDoc.getElementById('bulb').setAttributeNS(null, 'fill', '#000');
-        svgDoc.getElementById('gauge').setAttributeNS(null, 'stroke', '#000');
-    }
-    else {
-        //svgThermometer.getElementById('bulb').setAttribute('fill', 'rgb(255,255,0)');
-        //svgThermometer.setAttributeNS(null, bulb, rgb(255,255,0));
-        //svgThermometer.style.fill = '#ff0';
-        //document.getElementById('bulb').setAttributeNS(null, fill, rgb(0,0,0));
-        svgDoc.getElementById('bulb').setAttributeNS(null, 'fill', '#ff0');
-        svgDoc.getElementById('gauge').setAttributeNS(null, 'stroke', '#ff0');
-    }
+    const step = 500 / 30;
+    let valid_temp = 0;
+    let len = 675;
+
+    //Get valid temperature
+    if(temp < -30) valid_temp = -30;
+    else if(temp > 30) valid_temp = 30;
+    else valid_temp = temp;
+    
+    //Get thermometer length
+    if (valid_temp <= 0 ) len = 675;
+    else if (valid_temp <= 30) len = Math.floor(675 - (step * valid_temp));
+
+    console.log('TEMPERATURE: ' + valid_temp);
+
+    //Get percentage for colour change
+    let percent = (valid_temp + 30) / 60;
+    console.log('Percent: ' + percent);
+    let colour = colorLerp(0x0000ff, 0xff0000, percent);
+    console.log('Colour: ' + colour);
+
+    svgDoc.getElementById('bulb').setAttributeNS(null, 'fill', colour);
+    svgDoc.getElementById('gauge').setAttributeNS(null, 'stroke', colour);
+    svgDoc.getElementById('gauge').setAttributeNS(null, 'd', 'M500,750L500,' + len);
 }
 
-function setSVG(temp) {
-    console.log('TEMPERATURE: ' + temp);
+function setSVG() {
+    let temp = Number(document.querySelector('#temperature').value);
     timer = 0;
     fauto = false;
     applySVG(temp);
@@ -49,11 +57,11 @@ function setSVG(temp) {
 setInterval(()=> {
     timer++;
     console.log('Timer: ' + timer);
-    if(timer >= 10) {
+    if(fauto || timer === 10) {
+        if(!fauto) document.querySelector('#temperature').value = 0;
         fauto = true;
-        document.querySelector('#temperature').value = 0;
-        applySVG(30);
+        applySVG(Math.floor(Math.random() * 60 - 30));
     }
 }, 1000);
 
-submitButton.addEventListener('click', setSVG(Number(document.querySelector('#temperature').value)));
+submitButton.addEventListener('click', setSVG);
